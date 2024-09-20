2a07196e6924466314e8d5b01093c2b3b041639f
Kévin Moreau <moreau.kevin1234@gmail.com>
2024-09-20 16:22:06 +0200

    Bug when add row in db

---
+++ src/Entity/Product.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductRepository::class)]
+class Product
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $namme = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getNamme(): ?string
+    {
+        return $this->namme;
+    }
+
+    public function setNamme(string $namme): static
+    {
+        $this->namme = $namme;
+
+        return $this;
+    }
+}

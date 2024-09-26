33f0891ebb916391c2cad0f74df86557c2dc7aa4
Kévin Moreau <moreau.kevin1234@gmail.com>
2024-09-26 15:03:49 +0200

    Try to do a query on the Product table

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,8 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Product;
-
+use App\Repository\ProductRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\Entity;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -21,12 +22,13 @@ class ProductController extends AbstractController
         ]);
     }
 
+
     #[Route('/product/createProduct', name: 'app_create_product')]
     public function createProduct(EntityManagerInterface $entityManager, ValidatorInterface $validator): Response
     {
         $product = new Product();
-        $product->setName('Keyboard');
-        $product->setDescription('Logitech');
+        $product->setName('mouse');
+        $product->setDescription('blablabla');
         $product->setNote(3);
         $errors = $validator->validate($product);
         if (count($errors) > 0) {
@@ -40,5 +42,39 @@ class ProductController extends AbstractController
             $entityManager->flush();
             return new Response('Saved new product with id '.$product->getId());
         }
+    }
+
+    #[Route('/product/searchByName/{name}', name:'product_searchName')]
+    public function showSearch (ProductRepository $productRepository,EntityManagerInterface $entityManager, string $name): Response
+    {
+        $products = $productRepository
+        ->findAll();
+        if (!$products) {
+            throw $this->createNotFoundException(
+                'No product found for id '.$name
+            );
+        }
+        $blablabla=implode(" ",$products);
+        echo $blablabla;
+        return new Response('Check out this great product: ');
+    }
+    
+    #[Route('/product/{id}', name: 'product_show')]
+    public function show(ProductRepository $productRepository, int $id): Response
+    {
+        $product = $productRepository
+        ->find($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException(
+                'No product found for id '.$id
+            );
+        }
+
+        return new Response('Check out this great product: '.$product->getName());
+
+        // or render a template
+        // in the template, print things with {{ product.name }}
+        // return $this->render('product/show.html.twig', ['product' => $product]);
     }
 }
